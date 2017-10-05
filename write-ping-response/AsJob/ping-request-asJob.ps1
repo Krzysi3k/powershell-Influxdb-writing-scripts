a1bc54333ab5097cce3952aa1aeb0d0c62b40d57
Krzysi3k <global.takeover@o2.pl>
2017-10-05 12:11:13 +0200

    Update ping-request-asJob.ps1

--- write-ping-response/AsJob/ping-request-asJob.ps1
+++ write-ping-response/AsJob/ping-request-asJob.ps1
@@ -29,7 +29,7 @@ function ping-machines
         {
             if($table[$i].ResponseTime -eq $null)
             {
-                # send metrics with minus value
+                # send metrics with minus value (grafana mappings)
                 $ms = -100
                 WriteTo-InfluxDB -database $database -measurement $measurement -ms $ms -alias $json.machines.alias[$i]
                 Write-Host "$($json.machines.alias[$i]) $ms"
